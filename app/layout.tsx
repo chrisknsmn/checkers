d9bf7ba1a2602be6ced249e59a459528c5e721cf
chrisknsmn <chrisknsmn@gmail.com>
2025-07-17 13:36:37 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Checkers",
+  description: "Checkers",
+};
 
 export default function RootLayout({
   children,
