b7fd70f1cd53312438a03442fd1d2c79efdca7f8
chrisknsmn <chrisknsmn@gmail.com>
2025-07-17 18:05:19 -0400

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 hover:cursor-pointer rounded-lg shadow-xs",
+  "inline-flex items-center justify-center text-sm font-medium transition-all disabled:opacity-50 disabled:pointer-events-none rounded-lg focus-visible:ring-ring/50 focus-visible:ring-2 outline-none hover:cursor-pointer",
   {
     variants: {
       variant: {
@@ -13,13 +12,12 @@ const buttonVariants = cva(
         outline: "bg-accent hover:bg-accent/50",
         secondary:
           "bg-accent hover:bg-accent/50 text-foreground font-bold text-lg",
-        ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 gap-1.5 px-3 has-[>svg]:px-2.5",
+        default: "h-9 px-4 py-2",
+        sm: "h-8 px-3",
         lg: "p-2 w-full",
         icon: "size-9",
       },
@@ -31,22 +29,16 @@ const buttonVariants = cva(
   }
 );
 
-function Button({
-  className,
-  variant,
-  size,
-  asChild = false,
-  ...props
-}: React.ComponentProps<"button"> &
+type ButtonProps = React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
-  }) {
-  const Comp = asChild ? Slot : "button";
+  };
 
+function Button({ className, variant, size, asChild, ...props }: ButtonProps) {
+  const Comp = asChild ? Slot : "button";
   return (
     <Comp
-      data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(buttonVariants({ variant, size }), className)}
       {...props}
     />
   );
